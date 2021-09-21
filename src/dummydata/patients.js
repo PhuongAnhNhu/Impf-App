0d191da0e33c168399f8e33229b0aaffdf1fdcdc
Anna Phuong Anh <phuong.nhu@endava.com>
2021-09-21 13:48:24 +0200

    Dummy data

---
+++ src/dummydata/patients.js
@@ -0,0 +1,38 @@
+const patients = 
+[
+    {
+     vorname: "Müller",
+     nachname: "Max",
+     versicherungsnummer: "abc123456",
+     kvNummer:"123456",
+     geschlecht: "mannlich",
+     gebDatum: "26.01.1998",
+    },
+    {
+     vorname: "Müster",
+     nachname: "Tim",
+     versicherungsnummer: "abc123457",
+     kvNummer:"123457",
+     geschlecht: "mannlich",
+     gebDatum: "05.01.1989",
+    },
+    {
+     vorname: "Puth",
+     nachname: "Min",
+     versicherungsnummer: "abc123458",
+     kvNummer:"123458",
+     geschlecht: "mannlich",
+     gebDatum: "26.11.1978",
+    },
+    {
+     vorname: "Otto",
+     nachname: "Lily",
+     versicherungsnummer: "abc123459",
+     kvNummer:"123459",
+     geschlecht: "mannlich",
+     gebDatum: "03.01.1970",
+    },
+    
+]
+
+export default patients;
--- src/dummydata/user.js
+++ src/dummydata/user.js
@@ -3,6 +3,7 @@ const user =
         name: 'Anna',
         email: 'trang@example.com',
         password: 123456,
+        userId: "anna1"
     }
 
 export default user;
