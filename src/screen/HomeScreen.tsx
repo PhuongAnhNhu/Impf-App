cb5dd0b459b884508ec2891143c4efbe71cbc85a
Anna Phuong Anh <phuong.nhu@endava.com>
2021-09-21 13:49:00 +0200

    Added Appointment table

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import appointments from '../dummydata/appointments';
 
 const HomeScreen = () => {
     return (
         <div>
-            HomeScreen
+            {/* Anzeige nur Termin für einen Tag */}
+            <h1>Termin von heute:</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <th></th>
+                    <th className="text-center">Versicherungsnummer</th>
+                    <th className="text-center">Name</th>
+                    <th className="text-center">Vorname</th>
+                    <th className="text-center">Datum</th>
+                    <th className="text-center">Uhrzeit</th>
+                    <th className="text-center">Artz</th>
+                </thead>
+                <tbody>
+                    {appointments.map((appointment, index) => {
+                        return (
+                            <tr key={appointment.impfterminId}>
+                                <td>{index + 1}</td>
+                                <td>{appointment.versicherungsnummer}</td>
+                                <td>{appointment.nachname}</td>
+                                <td>{appointment.vorname}</td>
+                                <td>{appointment.datum}</td>
+                                <td>{appointment.datum}</td>
+                                <td>{appointment.arzt}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+            <h1>Anzahl des Impfstoff: </h1>
         </div>
-    )
-}
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
