ff1c751567bcee64a11c6cde0d960d4fa0bace78
Anna Phuong Anh <phuong.nhu@endava.com>
2021-09-22 13:34:28 +0200

    Appointment form

--- src/screen/Appointment.tsx
+++ src/screen/Appointment.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Box, FormControl, FormLabel, Input, InputLabel, Button, Autocomplete } from '@mui/material';
+import patients from '../dummydata/patients'
 
 const Appointment = () => {
     return (
-        <div>
-            Appointment
-        </div>
-    )
-}
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <FormControl>
+                <FormLabel component="legend">Appointment</FormLabel>
+                {/* TODO: Autocomplete if have time */}
+                <FormControl>
+                    <InputLabel htmlFor="Vorname">Vorname</InputLabel>
+                    <Input id="Vorname" aria-describedby="my-helper-text" />
+                </FormControl>
 
-export default Appointment
+                <FormControl>
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="Nachname">Nachname</InputLabel>
+                    <Input sx={{ width: '25rem' }} id="Nachname" aria-describedby="my-helper-text" />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="Versicherungsnummer">Versicherungsnummer</InputLabel>
+                    <Input id="Versicherungsnummer" aria-describedby="my-helper-text" />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="Artz">Artz</InputLabel>
+                    <Input id="Artz" aria-describedby="my-helper-text" />
+                </FormControl>
+
+                <Button sx={{ marginTop: '2rem' }} variant="outlined">
+                    Speichern
+                </Button>
+            </FormControl>
+        </Box>
+    );
+};
+
+export default Appointment;
