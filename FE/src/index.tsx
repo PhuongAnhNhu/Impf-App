8f0c941dff05dc5ebe53ae63d59ad406726842de
Anna Phuong Anh <phuong.nhu@endava.com>
2021-10-28 22:08:41 +0200

    Setup Store

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
-
---
+++ FE/src/reducers/users.tsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export interface UserState {
+    users: any[];
+    loading: boolean;
+    loggedIn: boolean;
+}
+
+const initialState: UserState = { users: [], loading: false, loggedIn: false };
+
+interface LoginPayload {
+    username: string;
+    password: string;
+}
+
+//Login
+export const login = createAsyncThunk('users/login', async (payload: LoginPayload) => {
+    const response = await axios.post('/login', payload);
+    return response.data;
+});
+
+export const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [login.fulfilled.type]: state => {
+            return {
+                ...state,
+                loggedIn: true,
+                loading: false,
+            };
+        },
+        [login.rejected.type]: state => {
+            return {
+                ...state,
+                loggedIn: false,
+                loading: false,
+            };
+        },
+        [login.pending.type]: state => {
+            return {
+                ...state,
+                loading: true,
+            };
+        },
+    },
+});
+
+export default usersSlice.reducer;
