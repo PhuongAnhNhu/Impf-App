af7bc93409a6441a125585cf461ca6dcc04d223a
Anna Phuong Anh <phuong.nhu@endava.com>
2021-11-01 08:16:38 +0100

    Set up Reducer and store

---
+++ FE/src/reducers/vaccineDoses.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState: VaccineDosesState = { vaccineDoses: [], loading: false };
+
+//GET vaccindoses
+export const getVaccineDoses = createAsyncThunk('patients/getVaccineDoses', async () => {
+    const response = await axios.get('/api/vaccine_doses');
+    return response.data.collection;
+});
+
+export const vaccineDosesSlice = createSlice({
+    name: 'patients',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        //getvaccindoses
+        [getVaccineDoses.fulfilled.type]: (state, action) => {
+            return {
+                ...state,
+                loading: false,
+                vaccineDoses: action.payload,
+            };
+        },
+        [getVaccineDoses.rejected.type]: state => {
+            return {
+                ...state,
+                loading: true,
+            };
+        },
+        [getVaccineDoses.pending.type]: state => {
+            return {
+                ...state,
+                loading: true,
+            };
+        },
+    },
+});
+
+export default vaccineDosesSlice.reducer;
--- FE/src/store.tsx
+++ FE/src/store.tsx
@@ -2,9 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './reducers/users';
 import patientsReducer from './reducers/patients';
 import appointmentsReducer from './reducers/appointments';
+import vaccineDosesReducer from './reducers/vaccineDoses';
 
 export const store = configureStore({
-    reducer: { usersState: usersReducer, patientsState: patientsReducer, appointmentsState: appointmentsReducer },
+    reducer: {
+        usersState: usersReducer,
+        patientsState: patientsReducer,
+        appointmentsState: appointmentsReducer,
+        vaccineDosesState: vaccineDosesReducer,
+    },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
