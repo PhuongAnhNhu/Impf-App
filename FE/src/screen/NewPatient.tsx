3b1cb426fc93ed0ac38ee85bc91cd4881f0cf842
Phuong Anh <nhuphuonganh001@..com>
2021-11-11 08:59:48 +0100

    patient form

--- FE/src/screen/NewPatient.tsx
+++ FE/src/screen/NewPatient.tsx
@@ -1,45 +1,112 @@
-import React from "react";
-import { Box, FormControl, FormLabel, Input, InputLabel, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Box, FormControl, FormLabel, Input, InputLabel, Button, TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import deLocale from "date-fns/locale/de";
+import DatePicker from "@mui/lab/DatePicker";
 
 const NewPatient = () => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [insurance, setInsurance] = useState("");
+    const [kkv, setKkv] = useState("");
+    const [gender, setGender] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [address, setAddress] = useState("");
+    const [zip, setZip] = useState("");
+    const [city, setCity] = useState("");
+
+    const localeMap = {
+        de: deLocale,
+    };
+
+    const maskMap = {
+        de: "__.__.____",
+    };
+
+    const saveHandler = () => {
+        console.log('save');
+    }
     return (
-        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+        <Box mt={10} sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
             <Box sx={{ width: "75%" }}>
-                <FormControl fullWidth margin="normal">
-                    <FormLabel>Patient</FormLabel>
-                    <FormControl margin="dense">
+            <FormControl fullWidth margin="normal">
+            <FormLabel component="legend">Neuer Patient</FormLabel>
+            <FormControl margin="dense">
                         <InputLabel htmlFor="Vorname">Vorname</InputLabel>
-                        <Input id="Vorname" aria-describedby="my-helper-text" />
+                        <Input
+                            id="Vorname"
+                            aria-describedby="my-helper-text"
+                            value={firstname}
+                            onChange={(e) => setFirstname(e.target.value)}
+                        />
                     </FormControl>
+
                     <FormControl margin="dense">
                         <InputLabel htmlFor="Nachname">Nachname</InputLabel>
-                        <Input id="Nachname" aria-describedby="my-helper-text" />
+                        <Input
+                            id="Nachname"
+                            aria-describedby="my-helper-text"
+                            value={lastname}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Versicherungsnummer">Versicherungsnummer</InputLabel>
-                        <Input id="Versicherungsnummer" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="username">Versicherungsnummer</InputLabel>
+                        <Input
+                            id="username"
+                            aria-describedby="my-helper-text"
+                            value={insurance}
+                            onChange={(e) => setInsurance(e.target.value)}
+                        />
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Artz">Geschlecht</InputLabel>
-                        <Input id="Artz" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="username">kvNummer</InputLabel>
+                        <Input id="username" aria-describedby="my-helper-text" value={kkv} onChange={(e) => setKkv(e.target.value)} />
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Artz">Geburtsdatum</InputLabel>
-                        <Input id="Artz" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="username">Geschlecht</InputLabel>
+                        <Input id="username" aria-describedby="my-helper-text" value={gender} onChange={(e) => setGender(e.target.value)} />
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Artz">Straße</InputLabel>
-                        <Input id="Artz" aria-describedby="my-helper-text" />
+                        <LocalizationProvider dateAdapter={AdapterDateFns} locale={localeMap["de"]}>
+                            <DatePicker
+                                mask={maskMap["de"]}
+                                label="Geburtsdatum"
+                                value={date}
+                                onChange={(newValue) => {
+                                    setDate(newValue);
+                                }}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Artz">PLZ</InputLabel>
-                        <Input id="Artz" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="username">Address</InputLabel>
+                        <Input
+                            id="username"
+                            aria-describedby="my-helper-text"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
                     </FormControl>
+
                     <FormControl margin="dense">
-                        <InputLabel htmlFor="Artz">Ort</InputLabel>
-                        <Input id="Artz" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="username">PLZ</InputLabel>
+                        <Input id="username" aria-describedby="my-helper-text" value={zip} onChange={(e) => setZip(e.target.value)} />
                     </FormControl>
-                    <Button sx={{ marginTop: "2rem" }} variant="outlined">
+
+                    <FormControl margin="dense">
+                        <InputLabel htmlFor="username">Stadt</InputLabel>
+                        <Input id="username" aria-describedby="my-helper-text" value={city} onChange={(e) => setCity(e.target.value)} />
+                    </FormControl>
+
+                    <Button onClick={saveHandler} sx={{ marginTop: "2rem" }} variant="outlined">
                         Speichern
                     </Button>
                 </FormControl>
