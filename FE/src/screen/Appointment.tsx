ebc459b0200740d2343018696afa72ef1ff77cf1
Anna Phuong Anh <phuong.nhu@endava.com>
2021-11-24 14:36:33 +0100

    Appointment Formular Autocomplete

--- FE/src/screen/Appointment.tsx
+++ FE/src/screen/Appointment.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Box, FormControl, FormLabel, Button, TextField, Autocomplete } from '@mui/material';
+import { Box, FormControl, FormLabel, Button, TextField, Autocomplete, CircularProgress } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import deLocale from 'date-fns/locale/de';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -10,6 +10,8 @@ import { RootState } from '../store';
 import { getPatients } from '../reducers/patients';
 import { postAppointment } from '../reducers/appointments';
 import { getVaccindosesNotAssigned } from '../reducers/vaccineDoses';
+import { getVaccines } from '../reducers/vaccines';
+import moment from 'moment';
 
 const Appointment = () => {
     const dispatch = useDispatch();
@@ -24,11 +26,11 @@ const Appointment = () => {
     };
 
     const [value, setValue] = React.useState<Date | null>(new Date());
-    const [firstname, setFirstname] = useState<String>('');
-    const [lastname, setLastname] = useState<String>('');
-    const [insurance, setInsurance] = useState<String>('');
-    const [vaccine, setVaccine] = useState<Number>();
-    const [vaccineType, setVaccineType] = useState<String>('');
+    const [firstname, setFirstname] = useState<string>('');
+    const [lastname, setLastname] = useState<string>('');
+    const [insurance, setInsurance] = useState<string>('');
+    const [vaccine, setVaccine] = useState<string>('');
+    const [vaccineType, setVaccineType] = useState<string>('');
 
     const patients: Patient[] = useSelector((state: RootState) => state.patientsState.patients);
     const patientInsurance = patients.map(item => item.insurance);
@@ -37,84 +39,101 @@ const Appointment = () => {
     const vaccinesTypeOptions = vaccines.map(item => item.name);
 
     const vaccineDoses: VaccineDose[] = useSelector((state: RootState) => state.vaccineDosesState.vaccineDosesNotAssigned);
-    const vaccineDosesList = vaccineDoses.map(item => item.id);
 
+    // Vaccine_Doses filter nach Impftermin 
+    const vaccineDosesNachDatum = vaccineDoses
+    .filter(e => moment.utc(value).format() <= moment.utc(e.expiresAt).format())
+    
+    let vaccineDosesList = vaccineDosesNachDatum.map(item => String(item.id));
+
+    if (vaccineType !== '') {
+        //Find vaccine hersteller
+        const vaccinesFilterID = vaccines.find(e => e.name === vaccineType);
+        //Find alle Vaccine Dose von dieser Hersteller
+        const vaccineListNachVaccineType = vaccineDosesNachDatum.filter(e => e.vaccine === vaccinesFilterID.id);
+        vaccineDosesList = vaccineListNachVaccineType.map(item => String(item.id));
+    }
 
     useEffect(() => {
         dispatch(getPatients());
         dispatch(getVaccindosesNotAssigned());
+        dispatch(getVaccines());
     }, []);
 
     return (
         <Box mt={10} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            <Box sx={{ width: '75%' }}>
-                <FormControl fullWidth>
-                    <FormLabel component="legend">Appointment</FormLabel>
-
-                    <FormControl margin="dense">
-                        <Autocomplete
-                            disableCloseOnSelect={false}
-                            value={insurance}
-                            options={patientInsurance}
-                            onChange={(e, newValue) => {
-                                setInsurance(newValue);
-                                setFirstname(patients.find(item => newValue === item.insurance)?.firstname || '');
-                                setLastname(patients.find(item => newValue === item.insurance)?.lastname || '');
-                            }}
-                            renderInput={params => <TextField {...params} label="Versicherungsnummer" required variant="standard" />}
-                        />
-                    </FormControl>
-
-                    <FormControl margin="dense">
-                        <TextField required variant="standard" label="Vorname" value={firstname} />
-                    </FormControl>
-
-                    <FormControl margin="dense">
-                        <TextField required variant="standard" label="Nachname" value={lastname} />
-                    </FormControl>
-
-                    <FormControl margin="dense">
-                        <LocalizationProvider dateAdapter={AdapterDateFns} locale={localeMap['de']}>
-                            <DesktopDateTimePicker
-                                mask={maskMap['de']}
-                                label="Uhrzeit"
-                                value={value}
-                                onChange={newValue => {
-                                    setValue(newValue);
+            {!patients.length && !vaccines.length ? (
+                <CircularProgress size={40} />
+            ) : (
+                <Box sx={{ width: '75%' }}>
+                    <FormControl fullWidth>
+                        <FormLabel component="legend">Appointment</FormLabel>
+                        <FormControl margin="dense">
+                            <Autocomplete
+                                disableCloseOnSelect={false}
+                                value={insurance}
+                                options={patientInsurance}
+                                onChange={(e, newValue) => {
+                                    setInsurance(newValue);
+                                    setFirstname(patients.find(item => newValue === item.insurance)?.firstname || '');
+                                    setLastname(patients.find(item => newValue === item.insurance)?.lastname || '');
                                 }}
-                                renderInput={params => <TextField {...params} />}
+                                renderInput={params => <TextField {...params} label="Versicherungsnummer" required variant="standard" />}
                             />
-                        </LocalizationProvider>
-                    </FormControl>
+                        </FormControl>
 
-                    <FormControl margin="dense">
-                        <Autocomplete
-                            disableCloseOnSelect={false}
-                            value={vaccineType}
-                            options={vaccinesTypeOptions}
-                            onChange={(e, newValue) => {
-                                setVaccineType(newValue);
-                            }}
-                            renderInput={params => <TextField {...params} label="Hersteller" required variant="standard" />}
-                        />
-                    </FormControl>
+                        <FormControl margin="dense">
+                            <TextField required variant="standard" label="Vorname" value={firstname} />
+                        </FormControl>
 
-                    <FormControl margin="dense">
-                        <Autocomplete
-                            disableCloseOnSelect={false}
-                            value={vaccine}
-                            options={vaccineDosesList}
-                            onChange={(e, newValue) => {
-                                setVaccine(Number(newValue));
-                            }}
-                            renderInput={params => <TextField {...params} label="Impfstoff" required variant="standard" />}
-                        />
+                        <FormControl margin="dense">
+                            <TextField required variant="standard" label="Nachname" value={lastname} />
+                        </FormControl>
+
+                        <FormControl margin="dense">
+                            <LocalizationProvider dateAdapter={AdapterDateFns} locale={localeMap['de']}>
+                                <DesktopDateTimePicker
+                                    mask={maskMap['de']}
+                                    label="Uhrzeit"
+                                    value={value}
+                                    onChange={newValue => {
+                                        setValue(newValue);
+                                    }}
+                                    renderInput={params => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </FormControl>
+
+                        <FormControl margin="dense">
+                            <Autocomplete
+                                autoHighlight={true}
+                                disableCloseOnSelect={false}
+                                value={vaccineType}
+                                options={vaccinesTypeOptions}
+                                onChange={(e, newValue) => {
+                                    setVaccineType(newValue);
+                                }}
+                                renderInput={params => <TextField {...params} label="Hersteller" required variant="standard" />}
+                            />
+                        </FormControl>
+
+                        <FormControl margin="dense">
+                            <Autocomplete
+                                disableCloseOnSelect={false}
+                                value={vaccine}
+                                options={vaccineDosesList}
+                                onChange={(e, newValue) => {
+                                    setVaccine(newValue);
+                                }}
+                                renderInput={params => <TextField {...params} label="Impfstoff" required variant="standard" />}
+                            />
+                        </FormControl>
+                        <Button sx={{ marginTop: '2rem' }} variant="outlined">
+                            Speichern
+                        </Button>
                     </FormControl>
-                    <Button sx={{ marginTop: '2rem' }} variant="outlined">
-                        Speichern
-                    </Button>
-                </FormControl>
-            </Box>
+                </Box>
+            )}
         </Box>
     );
 };
